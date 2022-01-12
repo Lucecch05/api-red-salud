69d0e480312f17b43c53f4c6eef63d0d9b33b23f
lucianocecchin05 <lucianocecchin05@gmail.com>
2022-01-12 18:40:14 -0300

    add database module + config

---
+++ src/config/config.service.ts
@@ -0,0 +1,49 @@
+import * as fs from 'fs';
+import { parse } from 'dotenv';
+
+export class ConfigService {
+   private readonly envConfig: {[key: string]: string};
+
+   constructor(){
+      const isDevelopmentEnv = process.env.NODE_ENV !== "production";
+      
+      if(isDevelopmentEnv){
+         const envFilePath = __dirname + '/../../.env';
+         const existsPath = fs.existsSync(envFilePath);
+
+         if (!existsPath) {
+            console.log('env file does not exist');
+            process.exit(0);
+         }
+
+         this.envConfig = parse(fs.readFileSync(envFilePath));
+
+
+
+      }
+      else{
+         this.envConfig = {
+            PORT: process.env.PORT,
+            HOST: '34.176.218.118',
+            USERNAME: 'root',
+            PASSWORD: 'infinitytech',
+            DATABASE: 'red_salud'
+         };
+      };
+
+
+   }
+
+   get(key: string): string {
+      return this.envConfig[key];
+   }
+
+}
+
+/* 
+ HOST: '34.176.218.118'
+USERNAME: 'root'
+PASSWORD: 'infinitytech'
+DATABASE: 'red_salud'
+PORT: 3306
+*/
---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,23 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+enum Estado {
+   Active = 'active',
+   Inactive = 'inactive'
+}
+
+@Entity('users')
+export class User extends BaseEntity {
+
+   @PrimaryGeneratedColumn('increment')
+   id: number;
+
+   @Column({type: 'varchar',unique: true, length: 25, nullable: false, name: 'usuario'})
+   username: string;
+
+   @Column({ type: 'varchar', length: 25, nullable: false })
+   password: string;
+
+   @Column({type: 'text'})
+   estado: Estado;
+   
+}

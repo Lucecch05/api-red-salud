97977d5912ac74a5d58b6c9b13404fc084b4a8fe
Lucecch05 <46322357+Lucecch05@users.noreply.github.com>
2022-02-22 21:49:19 -0300

    update main.js

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.SERVER_PORT || 5000);
   console.log("SERVER ON - PORT: ", AppModule.port);
 }
 bootstrap();

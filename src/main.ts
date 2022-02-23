cf146578fba71eaac5998a9e733c5555689c89a4
Lucecch05 <46322357+Lucecch05@users.noreply.github.com>
2022-02-23 16:06:21 -0300

    enable cors app module

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(process.env.SERVER_PORT || 5000);
   console.log("SERVER ON - PORT: ", AppModule.port);
